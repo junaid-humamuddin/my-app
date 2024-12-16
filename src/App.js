a1dd39e3c781927cef4a3591c1c1a73bce63a5e4
Mohammed Humamuddin Junaid <mhjunaid1995@gmail.com>
2024-12-16 18:16:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   React.useEffect(() => {
     fetch("/api")
       .then((res) => res.json())
-      .then((data) => setData(data.message));
+      .then((data) => setData(data.msg));
   }, []);
 
   return (
