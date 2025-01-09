7ee969badea65b90c4f5b2c7cb869e365ef971e4
Mohammed Humamuddin Junaid <mhjunaid1995@gmail.com>
2025-01-09 15:22:29 +0530

    sending the token as response

--- functions/controller/login.js
+++ functions/controller/login.js
@@ -5,7 +5,8 @@ exports.login = asyncHandler(async (req, res) => {
     const { email } = req.body;
     console.log(req.body);
     const user = {firstname : 'abc', lastname : 'xyx', id : '123', email}
-    res.status(200).cookie('token', generateToken(user)).json({ status: true, message: 'User logged in successfully', user });
+    // res.status(200).cookie('token', generateToken(user)).json({ status: true, message: 'User logged in successfully', user });
+    res.status(200).json({ token: generateToken(user), status: true, message: 'User logged in successfully', user });
 });
 
 const generateToken = ({ email, firstname, lastname, id }) => {
